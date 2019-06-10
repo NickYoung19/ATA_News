6e9661ac6b88a911f06cdfb6657b961f1fcdc335
NickyangPeng <pengyang19@163.com>
2019-06-10 13:44:13 +0800

    Realizes the register logics on front-end and fixed the front-end blur focus bug

--- info/static/news/js/main.js
+++ info/static/news/js/main.js
@@ -33,19 +33,27 @@ $(function(){
 
 
 	// 点击输入框，提示文字上移
-	$('.form_group').on('click focusin',function(){
-		$(this).children('.input_tip').animate({'top':-5,'font-size':12},'fast').siblings('input').focus().parent().addClass('hotline');
-	})
+	// $('.form_group').on('click focusin',function(){
+	// 	$(this).children('.input_tip').animate({'top':-5,'font-size':12},'fast').siblings('input').focus().parent().addClass('hotline');
+	// })
+    //
+	// // 输入框失去焦点，如果输入框为空，则提示文字下移
+	// $('.form_group input').on('blur focusout',function(){
+	// 	$(this).parent().removeClass('hotline');
+	// 	var val = $(this).val();
+	// 	if(val=='')
+	// 	{
+	// 		$(this).siblings('.input_tip').animate({'top':22,'font-size':14},'fast');
+	// 	}
+	// })
+    $('.form_group').on('click',function(){
+        $(this).children('input').focus()
+    })
 
-	// 输入框失去焦点，如果输入框为空，则提示文字下移
-	$('.form_group input').on('blur focusout',function(){
-		$(this).parent().removeClass('hotline');
-		var val = $(this).val();
-		if(val=='')
-		{
-			$(this).siblings('.input_tip').animate({'top':22,'font-size':14},'fast');
-		}
-	})
+    $('.form_group input').on('focusin',function(){
+        $(this).siblings('.input_tip').animate({'top':-5,'font-size':12},'fast')
+        $(this).parent().addClass('hotline');
+    })
 
 
 	// 打开注册框
@@ -144,6 +152,27 @@ $(function(){
         }
 
         // 发起注册请求
+        var params = {
+            "mobile": mobile,
+            "smscode": smscode,
+            "password": password
+        }
+        $.ajax({
+            url: "/passport/register",
+            type: "post",
+            data: JSON.stringify(params),
+            contentType: "application/json",
+            success: function (response) {
+                if (response.errno == "0") {
+                    // 注册成功,刷新页面
+                    location.reload();
+                }else{
+                    alert(response.errmsg)
+                    $("#register-password-err").html(response.errmsg)
+                    $("#register-password-err").show()
+                }
+            }
+        })
 
     })
 })
