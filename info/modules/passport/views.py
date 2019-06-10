b73dedf00b5e161d0af0cf85e4e8bb72211ad92a
NickyangPeng <pengyang19@163.com>
2019-06-10 10:46:10 +0800

    realized image code

--- info/modules/passport/views.py
+++ info/modules/passport/views.py
@@ -1,3 +1,11 @@
+from flask import request
+from flask import current_app
+from flask import abort
+from flask import make_response
+
+from info import redis_store
+from info import constants
+from info.utils.captcha.captcha import captcha
 from . import passport_blu
 
 
@@ -7,4 +15,21 @@ def get_image_code():
     Generate a image code and return
     :return:
     """
-    pass
+    # Get url params with request.args method
+    image_code_id = request.args.get('imageCodeId', None)
+    if not image_code_id:
+        return abort(404)
+    # Generate a image code
+    name, text, image = captcha.generate_captcha()
+    try:
+        # Save image code to redis
+        redis_store.set('imageCodeId_' + image_code_id, text, constants.IMAGE_CODE_REDIS_EXPIRES)
+    except Exception as e:
+        current_app.logger.error(e)
+        abort(500)
+
+    # Return image code to browser
+    response = make_response(image)
+    # Set response header data type
+    response.headers['Content-Type'] = 'image/jpg'
+    return response
